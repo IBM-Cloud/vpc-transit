e6e0677a2d1bf427a71c4156701ff538fd83f310
Powell Quiring <powellquiring@gmail.com>
2023-01-23 08:39:27 -0800

    upgrade to latest ubuntu

--- config_tf/variables.tf
+++ config_tf/variables.tf
@@ -34,7 +34,7 @@ variable "profile" {
   default = "cx2-2x4"
 }
 variable "image_name" {
-  default = "ibm-ubuntu-18-04-1-minimal-amd64-2"
+  default = "ibm-ubuntu-22-04-1-minimal-amd64-3"
 }
 
 # firewall implementation
--- py/test_transit.py
+++ py/test_transit.py
@@ -256,7 +256,7 @@ re_host_command_has_address = re.compile(r"(?s).*? has address ([0-9\.]+)")
 
 def fip_resolves_hostname(fip, dns_name):
     """ssh to fip and resolve the hostname"""
-    ret = command_on_fip(fip, ["systemd-resolve", "--flush-caches"])
+    ret = command_on_fip(fip, ["resolvectl", "flush-caches"])
     ret.assert_for_test()
     ret = command_on_fip(fip, ["host", dns_name])
     ret.assert_for_test()
